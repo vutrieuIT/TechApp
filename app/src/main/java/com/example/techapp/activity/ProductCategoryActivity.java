218812e185dcc5a563048e2fed8bf95a5acab042
Trieu Vu <vutrieu2002@gmail.com>
2023-05-11 20:03:41 +0700

    giao dien ưng dung

---
+++ app/src/main/java/com/example/techapp/activity/ProductCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.techapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_category);
+    }
+}
